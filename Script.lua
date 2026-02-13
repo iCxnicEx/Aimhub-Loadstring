b327da30b5d1017a7cdd365c38ed137f93c9d375
NovaModz <55899176+iCxnicEx@users.noreply.github.com>
2026-02-13 11:59:40 -0500

    Update launcher URL in Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/iCxnicEx/hub/refs/heads/main/LAUNCHER", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iCxnicEx/hub/refs/heads/main/Premium-Launcher", true))()
